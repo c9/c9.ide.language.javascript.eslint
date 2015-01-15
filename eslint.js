36ebfb78da2ad550702f276da93174208f065ee9
Lennart Kats <lclkats@gmail.com>
2015-01-15 16:32:57 +0100

    Move worker files into a separate directory

--- eslint.js
+++ eslint.js
@@ -12,7 +12,7 @@ define(function(require, exports, module) {
     function main(options, imports, register) {
         var language = imports.language;
 
-        language.registerLanguageHandler("plugins/c9.ide.language.javascript.eslint/eslint_worker");
+        language.registerLanguageHandler("plugins/c9.ide.language.javascript.eslint/worker/eslint_worker");
         
         register(null, {});
     }
