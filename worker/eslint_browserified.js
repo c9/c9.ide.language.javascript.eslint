dde8c5708d0b9ee07a70941d3f5b48ce85936114
Lennart Kats <lclkats@gmail.com>
2015-05-05 16:48:47 +0200

    Don't override global require

--- worker/eslint_browserified.js
+++ worker/eslint_browserified.js
@@ -1,4 +1,7 @@
-require=(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+// eslint 0.20.0, browserified with 'npm run browserify',
+// then wrapped (function() { var ... })();
+(function(){
+var require=(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 /**
  * @fileoverview A factory for creating AST nodes
  * @author Fred K. Schott
@@ -32461,3 +32464,4 @@ exports.PLUGIN_NAME_PREFIX = PLUGIN_NAME_PREFIX;
 
 },{}]},{},[133])(133)
 });
+})();
